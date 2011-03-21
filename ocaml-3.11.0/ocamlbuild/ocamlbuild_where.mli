e4c66c49dd7a2650ab0654fd9a6ca5eb05891d70
Perry Wagle <wagle@mac.com>
2011-03-21 09:42:27 -0700

    tar xf ocaml-3.11.0.tar.gz

---
+++ ocaml-3.11.0/ocamlbuild/ocamlbuild_where.mli
@@ -0,0 +1,17 @@
+(***********************************************************************)
+(*                             ocamlbuild                              *)
+(*                                                                     *)
+(*  Nicolas Pouillard, Berke Durak, projet Gallium, INRIA Rocquencourt *)
+(*                                                                     *)
+(*  Copyright 2007 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id: ocamlbuild_where.mli,v 1.2 2007/10/08 14:19:34 doligez Exp $ *)
+
+(* Original author: Nicolas Pouillard *)
+
+val bindir : string ref
+val libdir : string ref
